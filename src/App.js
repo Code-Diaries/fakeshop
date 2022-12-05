769ae98e34b580083dc01a9c6b325439ecf3e118
Esra Ocak <eesraunal93@gmail.com>
2022-12-05 19:12:31 +0300

    edit

--- src/App.js
+++ src/App.js
@@ -2,11 +2,12 @@ import React from 'react';
 import logo from './logo.svg';
 import { Counter } from './features/counter/Counter';
 import './App.css';
+import Footer from './components/Footer/Footer';
 
 function App() {
   return (
     <div className="App">
-      hello
+      <Footer/>
     </div>
   );
 }
