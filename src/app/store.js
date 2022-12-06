1405ea047579c2ad0b7431ea42a30104e556363a
aybike <aybikesayar@gmail.com>
2022-12-06 20:26:34 +0300

    searching

--- src/app/store.js
+++ src/app/store.js
@@ -2,13 +2,15 @@ import { configureStore } from "@reduxjs/toolkit";
 import categoryReducer from "../features/categorySlice/categorySlice";
 import productReducer from "../features/productSlice/productSlice"
 import filterReducer from "../features/filterSlice/filterSlice"
+import searchReducer from "../features/searchSlice/searchSlice";
 
 
 const store = configureStore({
   reducer: {
     product: productReducer,
     category: categoryReducer,
-    filter: filterReducer
+    filter: filterReducer,
+    search: searchReducer,
 
   },
 });
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -6,23 +6,75 @@ import CardContent from '@mui/material/CardContent';
 import CardMedia from '@mui/material/CardMedia';
 import Button from '@mui/material/Button';
 import Box from '@mui/material/Box';
-import Typography from '@mui/material/Typography';
 import InputLabel from '@mui/material/InputLabel';
 import MenuItem from '@mui/material/MenuItem';
 import FormControl from '@mui/material/FormControl';
-import Select, { SelectChangeEvent } from '@mui/material/Select';
+import Select from '@mui/material/Select';
+import { styled, alpha } from '@mui/material/styles';
+import InputBase from '@mui/material/InputBase';
+import IconButton from '@mui/material/IconButton';
+import Typography from '@mui/material/Typography';
+import MenuIcon from '@mui/icons-material/Menu';
+import SearchIcon from '@mui/icons-material/Search';
 import { useDispatch, useSelector } from 'react-redux';
-import { getProduct, setProduct } from '../../features/productSlice/productSlice';
+import { getProduct } from '../../features/productSlice/productSlice';
 import { getCategory, setChoosen } from '../../features/categorySlice/categorySlice';
 import { getFilter } from '../../features/filterSlice/filterSlice';
+import { setSearch } from '../../features/searchSlice/searchSlice';
+
+const Search = styled('div')(({ theme }) => ({
+    position: 'relative',
+    borderRadius: theme.shape.borderRadius,
+    backgroundColor: alpha(theme.palette.common.white, 0.15),
+    '&:hover': {
+        backgroundColor: alpha(theme.palette.common.white, 0.25),
+    },
+    marginLeft: 0,
+    width: '100%',
+    [theme.breakpoints.up('sm')]: {
+        marginLeft: theme.spacing(1),
+        width: 'auto',
+    },
+}));
+
+const SearchIconWrapper = styled('div')(({ theme }) => ({
+    padding: theme.spacing(0, 2),
+    height: '100%',
+    position: 'absolute',
+    pointerEvents: 'none',
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+}));
+
+const StyledInputBase = styled(InputBase)(({ theme }) => ({
+    color: 'inherit',
+    '& .MuiInputBase-input': {
+        padding: theme.spacing(1, 1, 1, 0),
+        // vertical padding + font size from searchIcon
+        paddingLeft: `calc(1em + ${theme.spacing(4)})`,
+        transition: theme.transitions.create('width'),
+        width: '100%',
+        [theme.breakpoints.up('sm')]: {
+            width: '12ch',
+            '&:focus': {
+                width: '20ch',
+            },
+        },
+    },
+}));
+
 
 
 
 const Header = () => {
     const { productList, loading, error } = useSelector((state) => state.product);
-    const { categoryList, choosen, loadingCategory, errorCategory } = useSelector((state) => state.category);
+    const { categoryList, loadingCategory, errorCategory } = useSelector((state) => state.category);
     const { filteredList, loadingFilter, errorFilter } = useSelector((state) => state.filter);
+    const { search } = useSelector((state) => state.search);
     const dispatch = useDispatch();
+    let displayArray = (filteredList.length ? filteredList : productList)
+
 
     useEffect(() => {
         dispatch(getProduct())
@@ -34,23 +86,44 @@ const Header = () => {
         dispatch(setChoosen(e.target.value))
         dispatch(getFilter())
         console.log("here")
+    }
 
+    const handleSubmit = (e) => {
+        e.preventDefault()
 
+        displayArray = displayArray.filter((item) => {
+            return (
+                item.title.includes(search) === true
+            )
+        })
 
+        console.log(displayArray)
 
     }
-    dispatch(setProduct(filteredList))
-
-
-    console.log(categoryList)
-    console.log(choosen);
-
-    console.log(filteredList);
     console.log(productList);
-
+    console.log(search);
+    console.log(displayArray)
 
     return (
         <>
+            <>
+                <Box sx={{ flexGrow: 1 }} onSubmit={handleSubmit} component="form">
+
+                    <Search >
+                        <SearchIconWrapper>
+                            <SearchIcon />
+                        </SearchIconWrapper>
+                        <StyledInputBase
+                            placeholder="Search…"
+                            inputProps={{ 'aria-label': 'search' }}
+                            onChange={(e) => dispatch(setSearch(e.target.value))}
+                            value={search}
+                        />
+                    </Search>
+
+
+                </Box>
+            </>
             <>
                 {errorCategory && (
                     <Typography variant="h3" color="error" align="center" mt={20}>
@@ -90,17 +163,17 @@ const Header = () => {
             </>
 
             <>
-                {error && (
+                {(errorFilter.length ? errorFilter : error) && (
                     <Typography variant="h3" color="error" align="center" mt={20}>
                         {error}
                     </Typography>
                 )}
-                {loading && (
+                {(loadingFilter.length ? loadingFilter : loading) && (
                     <Box display="flex" alignItems="center" justifyContent="center">
                         loading
                     </Box>
                 )}
-                {!loading && (
+                {!(loadingFilter.length ? loadingFilter : loading) && (
                     <Box
                         xs={{ d: "flex" }}
                         display="flex"
@@ -108,7 +181,7 @@ const Header = () => {
                         justifyContent="space-evenly"
                         flexWrap="wrap"
                     >
-                        {productList?.map((item, index) => (
+                        {displayArray?.map((item, index) => (
                             <Card sx={{ maxWidth: 345, maxHeight: 700, minHeight: 700, margin: 2 }} key={index}>
                                 <CardMedia
                                     component="img"
