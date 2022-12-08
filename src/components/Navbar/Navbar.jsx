d17e8a6cb6b6c12299bfda8cac44cbe60f13fc37
g-umac <gamzeumac49@gmail.com>
2022-12-08 19:40:33 +0100

    created sub component

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,68 +1,18 @@
 import * as React from 'react';
-import { styled, alpha } from '@mui/material/styles';
-import AppBar from '@mui/material/AppBar';
-import Box from '@mui/material/Box';
-import Toolbar from '@mui/material/Toolbar';
-import IconButton from '@mui/material/IconButton';
-import Typography from '@mui/material/Typography';
-import InputBase from '@mui/material/InputBase';
-import Badge from '@mui/material/Badge';
-import MenuItem from '@mui/material/MenuItem';
-import Menu from '@mui/material/Menu';
-import MenuIcon from '@mui/icons-material/Menu';
-import SearchIcon from '@mui/icons-material/Search';
+ import IconButton from '@mui/material/IconButton';
+import {Box, AppBar, Badge, Typography, MenuItem, Menu, Toolbar} from '@mui/material';
+import MenuIcon from '@mui/icons-material/Menu'; 
 import AccountCircle from '@mui/icons-material/AccountCircle';
 import MoreIcon from '@mui/icons-material/MoreVert';
 import FavoriteBorderIcon from '@mui/icons-material/FavoriteBorder';
 import AddShoppingCartIcon from '@mui/icons-material/AddShoppingCart';
 import LightModeIcon from '@mui/icons-material/LightMode';
-import {useNavigate } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';  
 
 
-const Search = styled('div')(({ theme }) => ({
-  position: 'relative',
-  borderRadius: theme.shape.borderRadius,
-  backgroundColor: alpha(theme.palette.common.white, 0.15),
-  '&:hover': {
-    backgroundColor: alpha(theme.palette.common.white, 0.25),
-  },
-  marginRight: theme.spacing(2),
-  marginLeft: 0,
-  width: '100%',
-  [theme.breakpoints.up('sm')]: {
-    marginLeft: theme.spacing(3),
-    width: 'auto',
-  },
-}));
+ const Navbar = () => { 
 
-const SearchIconWrapper = styled('div')(({ theme }) => ({
-  padding: theme.spacing(0, 2),
-  height: '100%',
-  position: 'absolute',
-  pointerEvents: 'none',
-  display: 'flex',
-  alignItems: 'center',
-  justifyContent: 'center',
-}));
-
-const StyledInputBase = styled(InputBase)(({ theme }) => ({
-  color: 'inherit',
-  '& .MuiInputBase-input': {
-    padding: theme.spacing(1, 1, 1, 0),
-    // vertical padding + font size from searchIcon
-    paddingLeft: `calc(1em + ${theme.spacing(4)})`,
-    transition: theme.transitions.create('width'),
-    width: '100%',
-    [theme.breakpoints.up('md')]: {
-      width: '20ch',
-    },
-  },
-}));
-
-const Navbar=()=>{
-
-
-    // const navigate = useNavigate();
+  // const navigate = useNavigate();
 
   const [anchorEl, setAnchorEl] = React.useState(null);
   const [mobileMoreAnchorEl, setMobileMoreAnchorEl] = React.useState(null);
@@ -104,17 +54,17 @@ const Navbar=()=>{
       open={isMenuOpen}
       onClose={handleMenuClose}
     >
-          <MenuItem onClick={handleMenuClose}>Login</MenuItem>
-          {/* user olduğunda Logout user yoksa Login yazacak */}
-      
+      <MenuItem onClick={handleMenuClose}>Login</MenuItem>
+      {/* user olduğunda Logout user yoksa Login yazacak */}
+
     </Menu>
   );
- 
-  
+
+
 
   const mobileMenuId = 'primary-search-account-menu-mobile';
-    const renderMobileMenu = (
-      
+  const renderMobileMenu = (
+
     <Menu
       anchorEl={mobileMoreAnchorEl}
       anchorOrigin={{
@@ -128,13 +78,13 @@ const Navbar=()=>{
         horizontal: 'right',
       }}
       open={isMobileMenuOpen}
-          onClose={handleMobileMenuClose}
-          
+      onClose={handleMobileMenuClose}
+
     >
       <MenuItem>
-        <IconButton size="large"  color="inherit">
-          <Badge  color="error">
-            <FavoriteBorderIcon/>
+        <IconButton size="large" color="inherit">
+          <Badge color="error">
+            <FavoriteBorderIcon />
           </Badge>
         </IconButton>
         <p>Favourites</p>
@@ -142,11 +92,11 @@ const Navbar=()=>{
       <MenuItem>
         <IconButton
           size="large"
-         
+
           color="inherit"
         >
           <Badge badgeContent={2} color="error">
-            <AddShoppingCartIcon/>
+            <AddShoppingCartIcon />
           </Badge>
         </IconButton>
         <p>Basket</p>
@@ -157,22 +107,22 @@ const Navbar=()=>{
           aria-label="account of current user"
           aria-controls="primary-search-account-menu"
           aria-haspopup="true"
-                  color="inherit"
-                    // onClick={() => navigate("/login")} 
-                    // redirect gelecek
+          color="inherit"
+        // onClick={() => navigate("/login")} 
+        // redirect gelecek
         >
           <AccountCircle />
         </IconButton>
-              <p >LogIn/LogOut</p>
-              
+        <p >LogIn/LogOut</p>
+
       </MenuItem>
     </Menu>
-    );
+  );
 
-  
+
   return (
     <Box sx={{ flexGrow: 1 }}>
-      <AppBar position="static" sx={{bgcolor:"warning.main"}}>
+      <AppBar position="static" sx={{ bgcolor: "warning.main" }}>
         <Toolbar>
           <IconButton
             size="large"
@@ -191,29 +141,23 @@ const Navbar=()=>{
           >
             FAKESHOP
           </Typography>
-          <Search>
-            <SearchIconWrapper>
-              <SearchIcon />
-            </SearchIconWrapper>
-            <StyledInputBase
-              placeholder="Search…"
-              inputProps={{ 'aria-label': 'search' }}
-            />
-          </Search>
-                  <Box sx={{ flexGrow: 1 }} />
+
+       
+
+          <Box sx={{ flexGrow: 1 }} />
           <Box sx={{ display: { xs: 'none', md: 'flex' } }}>
             <IconButton size="large" aria-label="show 4 new mails" color="inherit">
               <Badge color="error">
-              <FavoriteBorderIcon/>
+                <FavoriteBorderIcon />
               </Badge>
             </IconButton>
             <IconButton
               size="large"
               aria-label="show 17 new notifications"
               color="inherit">
-            <Badge badgeContent={2} color="error">  
+              <Badge badgeContent={2} color="error">
                 {/* burdaki badge contente state gelecek */}
-              <AddShoppingCartIcon/>
+                <AddShoppingCartIcon />
               </Badge>
             </IconButton>
             <IconButton
@@ -225,15 +169,15 @@ const Navbar=()=>{
               onClick={handleProfileMenuOpen}
               color="inherit"
             >
-             <AccountCircle />
-                          
+              <AccountCircle />
+
             </IconButton>
-             <IconButton size="large"
+            <IconButton size="large"
               aria-label="show 17 new notifications"
-            color="inherit">
-                          <LightModeIcon /> 
-                          {/* karanlıksa light mode aydılıksa dark mode ikon */}
-                      </IconButton>
+              color="inherit">
+              <LightModeIcon />
+              {/* karanlıksa light mode aydılıksa dark mode ikon */}
+            </IconButton>
 
           </Box>
           <Box sx={{ display: { xs: 'flex', md: 'none' } }}>
