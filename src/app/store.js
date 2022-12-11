78104b136db591c81f7f618afd912193e4f0a004
aybike <aybikesayar@gmail.com>
2022-12-11 22:00:54 +0300

    cmmt

--- src/app/store.js
+++ src/app/store.js
@@ -1,31 +1,22 @@
 import { configureStore } from "@reduxjs/toolkit";
-<<<<<<< HEAD
 import authReducer from "../features/authSlice";
 
-=======
 import categoryReducer from "../features/categorySlice/categorySlice";
 import productReducer from "../features/productSlice/productSlice"
 import filterReducer from "../features/filterSlice/filterSlice"
 import searchReducer from "../features/searchSlice/searchSlice";
 import favoriteSliceReducer from "../features/favoriteSlice/favoriteSlice";
->>>>>>> 47ae9b059f70c6e5e8489e5608be2f48b60a20e7
 
 
 const store = configureStore({
   reducer: {
-<<<<<<< HEAD
-    auth : authReducer,
-   
-  },
-});
-=======
     product: productReducer,
     category: categoryReducer,
     filter: filterReducer,
     search: searchReducer,
+    auth: authReducer,
     favorite: favoriteSliceReducer
 
   },
 });
 export default store;
->>>>>>> 47ae9b059f70c6e5e8489e5608be2f48b60a20e7
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,20 +1,9 @@
 import React from 'react'
-<<<<<<< HEAD
-import AllProduct from '../components/AllProduct'
-import Head from '../components/Head'
-
-const Home = () => {
-    return (
-        <div>
-            
-        </div>
-=======
 import Header from '../components/Header/Header'
 
 const Home = () => {
     return (
         <Header />
->>>>>>> 47ae9b059f70c6e5e8489e5608be2f48b60a20e7
     )
 }
 
