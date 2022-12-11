4629f61367e8865056011a2ef084c36f5929e59b
Esra Ocak <eesraunal93@gmail.com>
2022-12-11 20:50:21 +0300

    update

--- src/App.js
+++ src/App.js
@@ -1,25 +1,11 @@
 import React from 'react';
-<<<<<<< HEAD
-import logo from './logo.svg';
-import { Counter } from './features/counter/Counter';
-import './App.css';
-import Footer from './components/Footer/Footer';
-=======
-
-
 import Navbar from './components/Navbar/Navbar';
 import Home from "./pages/Home"
->>>>>>> 9056851bf47ea71f937a7653d64b5fe137dec903
-
 function App() {
   return (
     <div className="App">
-<<<<<<< HEAD
-      <Footer/>
-=======
       <Navbar />
       <Home/>
->>>>>>> 9056851bf47ea71f937a7653d64b5fe137dec903
     </div>
   );
 }
