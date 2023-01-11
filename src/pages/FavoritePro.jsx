1d70ff5a4aa7b770e05f140489964860a6507755
aybike <aybikesayar@gmail.com>
2023-01-11 21:51:24 +0300

    favorite done

--- src/pages/FavoritePro.jsx
+++ src/pages/FavoritePro.jsx
@@ -1,7 +1,12 @@
 import React from 'react'
 import { useSelector } from 'react-redux'
-import { Typography, Card, Button, CardMedia, CardContent, CardActions } from '@mui/material';
-import { Box } from '@mui/system';
+import { Typography, Card, CardContent, CardActions, CardMedia } from '@mui/material';
+import RemoveRedEyeIcon from '@mui/icons-material/RemoveRedEye';
+import ShoppingBasketIcon from '@mui/icons-material/ShoppingBasket';
+import Box from '@mui/material/Box';
+import Rating from '@mui/material/Rating';
+
+
 
 const FavoritePro = () => {
     const { favoriteList } = useSelector(state => state.favorite)
@@ -17,27 +22,35 @@ const FavoritePro = () => {
                 flexWrap="wrap">
                 {favoriteList?.map((item, index) => {
                     return (
-                        <Card sx={{ maxWidth: 345, maxHeight: 450, minHeight: 450, margin: 2 }} key={index}>
+                        <Card sx={{ maxWidth: 250, minWidth: 250, maxHeight: 350, minHeight: 350, margin: 2, padding: 2, background: "##F5F5F5" }} key={index}>
+
                             <CardMedia
                                 component="img"
                                 alt={item?.title}
-                                height="250"
+                                height="200"
                                 image={item?.image}
-                                style={{ objectFit: "contain", width: 350, height: 250, }}
+                                style={{ objectFit: "contain", width: 250, height: 180, background: "white" }}
                             />
                             <CardContent>
-                                <Typography gutterBottom variant="h6" component="div" >
-                                    {item?.title}
-                                </Typography>
-                                <Typography variant="h6" color="primary">
-                                    Rate: {item?.rating?.rate}
-                                </Typography>
-                                <Typography variant="h3" color="orange">
+                                <div style={{ display: "flex", justifyContent: "space-between" }}>
+                                    <Typography gutterBottom variant="h6" component="div" height={40} style={{ fontSize: ".6rem", marginTop: ".5rem" }}>
+                                        {item?.title}
+                                    </Typography>
+
+                                    <Typography variant="h6" color="primary"   >
+                                        <Rating name="read-only" value={item?.rating?.rate} readOnly style={{ fontSize: "1rem", marginTop: "0" }} />
+                                    </Typography>
+                                </div>
+
+                            </CardContent>
+                            <CardActions style={{ marginTop: "0", display: "flex", justifyContent: "space-between" }}>
+                                <Typography variant="h5" color="orange" style={{ fontSize: "1rem", fontWeight: "600" }}>
                                     {item?.price} TL
                                 </Typography>
-                            </CardContent>
-                            <CardActions>
-                                <Button size="small">Sepete Ekle</Button>
+                                <div style={{ background: "#EDCF65", padding: "1rem", width: "4rem", display: "flex", justifyContent: "space-evenly" }}>
+                                    <RemoveRedEyeIcon />
+                                    <ShoppingBasketIcon />
+                                </div>
                             </CardActions>
                         </Card>
                     )
